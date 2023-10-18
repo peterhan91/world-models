611c9a8674cc6c2e6c87d6cc8c8ff83517545831
peterhan91 <petertianyuhan@hotmail.com>
2023-10-18 11:37:37 +0200

    update pccs

--- pccs.py
+++ pccs.py
@@ -3,7 +3,7 @@ import numpy as np
 import warnings
 from rdkit import Chem
 
-def count_carbon_atoms(smiles, atom="C"):
+def count_carbon_atoms(smiles, atom_name="C"):
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
         mol = Chem.MolFromSmiles(smiles)
@@ -12,11 +12,11 @@ def count_carbon_atoms(smiles, atom="C"):
         if mol is None:  # Invalid SMILES string
             return np.nan
         
-        atoms = [atom for atom in mol.GetAtoms() if atom.GetSymbol() == atom]
+        atoms = [atom for atom in mol.GetAtoms() if atom.GetSymbol() == atom_name]
         return len(atoms)
 
-df_pcc = pd.read_csv('../drugs/pccs-pubchem/test.tsv', sep='\t')
-df_pcc_tr = pd.read_csv('../drugs/pccs-pubchem/train.tsv', sep='\t')
+df_pcc = pd.read_csv('../../Datasets/drugs/pccs-pubchem/test.tsv', sep='\t')
+df_pcc_tr = pd.read_csv('../../Datasets/drugs/pccs-pubchem/train.tsv', sep='\t')
 
 df_pcc_test = df_pcc[~df_pcc['Name'].isin(df_pcc_tr['Name'].tolist())]
 df_pcc_tr['is_test'] = False
@@ -28,13 +28,11 @@ df_pcc_tr['No. N'] = df_pcc_tr['SMILES'].apply(lambda x: count_carbon_atoms(x, a
 df_pcc_test['No. N'] = df_pcc_test['SMILES'].apply(lambda x: count_carbon_atoms(x, atom='N'))
 df_pcc_tr['No. O'] = df_pcc_tr['SMILES'].apply(lambda x: count_carbon_atoms(x, atom='O'))
 df_pcc_test['No. O'] = df_pcc_test['SMILES'].apply(lambda x: count_carbon_atoms(x, atom='O'))
-df_pcc_tr['No. H'] = df_pcc_tr['SMILES'].apply(lambda x: count_carbon_atoms(x, atom='H'))
-df_pcc_test['No. H'] = df_pcc_test['SMILES'].apply(lambda x: count_carbon_atoms(x, atom='H'))
 
 df_pcc_tr = df_pcc_tr.dropna()
 df_pcc_test = df_pcc_test.dropna()
-df_pcc_test = df_pcc_test.filter(['Name', 'SMILES', 'No. C', 'No. N', 'No. O', 'No. H', 'is_test'])
-df_pcc_tr = df_pcc_tr.filter(['Name', 'SMILES', 'No. C', 'No. N', 'No. O', 'No. H', 'is_test'])
+df_pcc_test = df_pcc_test.filter(['Name', 'SMILES', 'No. C', 'No. N', 'No. O', 'is_test'])
+df_pcc_tr = df_pcc_tr.filter(['Name', 'SMILES', 'No. C', 'No. N', 'No. O', 'is_test'])
 df = pd.concat([df_pcc_tr, df_pcc_test])
 
-df.to_csv('data/entity_datasets/pccs.csv', index=False)
+df.to_csv('./data/entity_datasets/pccs.csv', index=False)
